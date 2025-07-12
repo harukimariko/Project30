c4f37ffcd366dd3a2360b7da779173e690645b79
marikoharuki <150422358+marikoharuki@users.noreply.github.com>
2025-07-12 13:41:14 +0900

    スクリプトの中身を記入

--- Assets/Script/CharacterStatus.cs
+++ Assets/Script/CharacterStatus.cs
@@ -1,6 +1,39 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CharacterStatus : MonoBehaviour
 {
+    public string _name { get; private set; } = "none";
+    [Range(1, 100)] public int _hpMax = 0;
+    public int _hp { get; private set; } = 1;
+    [SerializeField] private List<GameObject> _destroySpawnObjectList;
+
+    private void Awake()
+    {
+        _hp = _hpMax;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void SetHp(int hp){_hp = hp; }
+    public void ApplyHp(int apply)
+    {
+        _hp += apply;
+
+        if (_hp <= 0)
+        {
+            foreach(GameObject obj in _destroySpawnObjectList)
+            {
+                Vector3 pos = transform.position + obj.transform.position;
+                Quaternion rot = obj.transform.rotation;
+
+                GameObject spawnObject = Instantiate(obj, pos, rot);
+            }
+        }
+    }
+    public void SetName(string name) { _name = name; }
 
 }
